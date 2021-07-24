52257f98594d5843578a6ab3fc4a4a5c8bf6077f
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-07-24 16:41:43 +0100

    edit: rename import

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/Paulo-Lopes-Estevao/webserver-example-go/http"
+import (
+	"github.com/Paulo-Lopes-Estevao/webserver-example-go/http"
+	"github.com/Paulo-Lopes-Estevao/webserver-example-go/model"
+)
 
 func main() {
 	webserver:=http.NewWebServer()
+	webserver.Products = &model.Products{}
 	webserver.Serve()
 }
